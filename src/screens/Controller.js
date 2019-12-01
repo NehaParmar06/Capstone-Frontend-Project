69010c11109889a439578f25856c55e2ec3ac692
Rohit Shinde <rohitshinde803@gmail.com>
2019-12-01 12:40:00 +0530

    Revert "Details Page added"
    This reverts commit 1b9dc6aac07b7b8d15173e569daa4173d542b1c5.

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Home from '../screens/home/Home';
 import Checkout from '../screens/checkout/Checkout';
-import Details from '../screens/details/Details';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class Controller extends Component {
@@ -15,7 +14,6 @@ class Controller extends Component {
                 <Router>
                     <Route exact path="/" render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
                     <Route exact path="/checkout" render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
-                    <Route exact path="/details" render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
                 </Router>
             </div>
         );

aa5be227f4d117cbcbfef4eec6b068ab5d99f97a
Rohit Shinde <rohitshinde803@gmail.com>
2019-12-01 12:43:31 +0530

    Details Page added
    - Displays restaurant basic information

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Home from '../screens/home/Home';
 import Checkout from '../screens/checkout/Checkout';
+import Details from '../screens/details/Details';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class Controller extends Component {
     constructor() {
         super();
-        this.baseUrl = "http://localhost:8080/api";
+        this.baseUrl = "http://localhost:8080/api/";
     }
     render() {
         return (
@@ -14,9 +15,11 @@ class Controller extends Component {
                 <Router>
                     <Route exact path="/" render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
                     <Route exact path="/checkout" render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
+                    <Route exact path="/details" render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
                 </Router>
             </div>
         );
+
     }
 }
 
